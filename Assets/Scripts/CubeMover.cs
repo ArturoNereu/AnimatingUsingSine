23360234aff7fdd78b0d17dbdd6823bd6e73aed9
Arturo Nereu <hdgam3r@gmail.com>
2019-01-15 19:50:56 -0600

    Added project

---
+++ Assets/Scripts/CubeMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMover : MonoBehaviour
+{
+    private Vector3 originalTransform;
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float amplitude;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalTransform = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = originalTransform + new Vector3(0, Mathf.Sin(Time.time * speed) * amplitude, 0);
+    }
+}
